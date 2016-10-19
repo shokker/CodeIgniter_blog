cd51a9fb63be1ae1b6067882c3856cc270fed996
shokker <lukas.danko1@gmail.com>
2016-10-19 15:15:08 +0200

    email utf-8

--- application/config/email.php
+++ application/config/email.php
@@ -6,6 +6,6 @@ $config['smtp_port'] = "465";
 $config['smtp_user'] = "lukas.danko1@gmail.com";
 $config['smtp_pass'] = "jcccdpbldygvgirn";
 $config['mailtype'] = "html";
-$config['charset'] = "iso-8859-1";
+$config['charset'] = "utf-8";
 $config['wordwrap'] = TRUE;
 
